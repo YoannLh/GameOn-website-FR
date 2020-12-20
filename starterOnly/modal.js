fa066d6c39fbf9f3047aa4db947a0df17a504d20
Yoann L'hostis <yolhostis@live.fr>
2020-12-20 18:39:30 +0100

    Ajout e la class Modal: issue 1 résolue

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,23 +1,56 @@
-function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+
+
+class Modal {
+
+	constructor() {
+
+		this.modalBg = document.querySelector(".bground");
+		this.modalBtn = document.querySelector(".modal-btn");
+		this.formData = document.querySelectorAll(".formData");
+		this.closeButton = document.getElementById("close");
+	}
+
+	displayModal() {
+
+		this.modalBtn.addEventListener("click", () => {
+
+			this.modalBg.style.display = "block";
+		});
+
+		this.closeButton.addEventListener("click", () => {
+
+			this.modalBg.style.display = "none";
+		})
+	}
 }
 
-// DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+const modal = new Modal();
 
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modal.displayModal();
 
-// launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
-}
+
+
+
+// function editNav() {
+//   var x = document.getElementById("myTopnav");
+//   if (x.className === "topnav") {
+//     x.className += " responsive";
+//   } else {
+//     x.className = "topnav";
+//   }
+// }
+
+// // DOM Elements
+// const modalbg = document.querySelector(".bground");
+// const modalBtn = document.querySelectorAll(".modal-btn");
+// const formData = document.querySelectorAll(".formData");
+
+// // launch modal event
+// modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// // launch modal form
+// function launchModal() {
+//   modalbg.style.display = "block";
+// }
 
 
